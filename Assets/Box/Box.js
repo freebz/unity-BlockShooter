cc38aea2178c4f51cf9d0ce899ee0ba79cab4c9e
FreeBz <freebz.dev@gmail.com>
2013-02-03 11:30:08 +0900

    소멸시간 지연하기

--- Assets/Box/Box.js
+++ Assets/Box/Box.js
@@ -2,16 +2,26 @@
 
 var explosionPrefab : GameObject;	// 폭팔 효과
 
+private var damaged : boolean;	// 데미지 입음
+private var killTimer : float;	// 소멸까지의 시간
+
 function Start () {
 
 }
 
 function Update () {
-
+	if (!damaged) return;
+	killTimer -= Time.deltaTime;
+	if (killTimer <= 0.0) {
+		// 효과를 내면서 자신의 게임 오브젝트 파괴
+		Instantiate(explosionPrefab, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
 }
 
 function ApplyDamage() {
-	// 효과를 내면서 자신의 게임 오브젝트 파괴
-	Instantiate(explosionPrefab, transform.position, transform.rotation);
-	Destroy(gameObject);
+	if (!damaged) {
+		damaged = true;
+		killTimer = 0.4;
+	}
 }
